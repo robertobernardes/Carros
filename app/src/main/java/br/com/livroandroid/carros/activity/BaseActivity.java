60dcef4f44be632b6d5cd5b2a90d986a6d58baa2
Roberto Bernardes <roberto_bernardes@hotmail.com>
2016-11-19 19:18:22 -0300

    Iniciando Projeto Carros

---
+++ app/src/main/java/br/com/livroandroid/carros/activity/BaseActivity.java
@@ -0,0 +1,19 @@
+package br.com.livroandroid.carros.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import br.com.livroandroid.carros.R;
+
+/**
+ * Created by Roberto on 19/11/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    // Configura a Toolbar
+    protected void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+}
---
+++ app/src/main/java/br/com/livroandroid/carros/fragments/BaseFragments.java
@@ -0,0 +1,9 @@
+package br.com.livroandroid.carros.fragments;
+
+import android.support.v4.app.Fragment;
+/**
+ * Created by Roberto on 19/11/2016.
+ */
+
+public class BaseFragments extends Fragment {
+}
