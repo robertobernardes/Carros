463a48fb07d1f7ad65728d16e4155ce38ecab77a
Roberto Bernardes <roberto_bernardes@hotmail.com>
2016-11-29 21:21:36 -0300

    Continuando Projeto Carros

--- app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
+++ app/src/main/java/br/com/livroandroid/carros/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.livroandroid.carros.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import br.com.livroandroid.carros.R;
+import br.com.livroandroid.carros.fragments.AboutDialog;
 import br.com.livroandroid.carros.fragments.CarrosFragment;
 
 public class MainActivity extends BaseActivity {
@@ -14,5 +18,22 @@ public class MainActivity extends BaseActivity {
         setupNavDrawer();
 
         replaceFragment(CarrosFragment.newInstance(R.string.todos));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            AboutDialog.showAbout(getSupportFragmentManager());
+            //toast("Clicou em Sobre");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
