8f014ee7990ac7c3902ba8ccdd4f80bebb639ee3
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2016-11-30 08:12:57 -0300

    .

--- app/src/main/java/br/com/livroandroid/carros/fragments/SiteLivroFragment.java
+++ app/src/main/java/br/com/livroandroid/carros/fragments/SiteLivroFragment.java
@@ -1,18 +1,52 @@
 package br.com.livroandroid.carros.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import br.com.livroandroid.carros.R;
 import livroandroid.lib.fragment.BaseFragment;
 
 public class SiteLivroFragment extends BaseFragment {
+    private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
+    private WebView webview;
+    private ProgressBar progress;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_site_livro, container, false);
+        webview = (WebView) view.findViewById(R.id.webview);
+        progress = (ProgressBar) view.findViewById(R.id.progress);
+        setWebViewClient(webview);
+        // Carrega a página
+        webview.loadUrl(URL_SOBRE);
         return view;
+    }
+
+    private void setWebViewClient(WebView webview) {
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+                super.onPageStarted(webview, url, favicon);
+                // Liga o progress
+                progress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView webview, String url) {
+                // Desliga o progress
+                progress.setVisibility(View.INVISIBLE);
+                // Termina a animação do Swipe to Refresh
+                //swipeLayout.setRefreshing(false);
+            }
+
+        });
     }
 }
