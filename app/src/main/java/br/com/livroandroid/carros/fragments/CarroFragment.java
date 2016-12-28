280a78ce085be22741018d9509ba1a04cfeab12b
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2016-12-28 08:10:23 -0300

    Continuando estudos android - Banco de Dados

--- app/src/main/java/br/com/livroandroid/carros/fragments/CarroFragment.java
+++ app/src/main/java/br/com/livroandroid/carros/fragments/CarroFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,9 +15,14 @@ import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
+import br.com.livroandroid.carros.CarrosApplication;
 import br.com.livroandroid.carros.R;
+import br.com.livroandroid.carros.activity.CarroActivity;
 import br.com.livroandroid.carros.domain.Carro;
+import br.com.livroandroid.carros.domain.CarroDB;
 import livroandroid.lib.fragment.BaseFragment;
+
+import static java.security.AccessController.getContext;
 
 public class CarroFragment extends BaseFragment {
     private Carro carro;
@@ -24,6 +32,7 @@ public class CarroFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_carro, container, false);
         carro = Parcels.unwrap(getArguments().getParcelable("carro"));
+        setHasOptionsMenu(true);
         return view;
     }
 
@@ -34,5 +43,27 @@ public class CarroFragment extends BaseFragment {
         setTextString(R.id.tDesc, carro.desc);
         final ImageView imgView = (ImageView) getView().findViewById(R.id.img);
         Picasso.with(getContext()).load(carro.urlFoto).fit().into(imgView);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_frag_carro, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_edit) {
+            toast("Editar: " + carro.nome);
+        } else if (item.getItemId() == R.id.action_delete) {
+            toast("Deletar: " + carro.nome);
+        } else if (item.getItemId() == R.id.action_share) {
+            toast("Compartilhar");
+        } else if (item.getItemId() == R.id.action_maps) {
+            toast("Mapa");
+        } else if (item.getItemId() == R.id.action_video) {
+            toast("Vídeo");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
